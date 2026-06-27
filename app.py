a6998cb9cc78a28b48e7423600cfd55e6114f4d4
Sergio Ospina <checho@Sergios-MacBook-Pro.local>
2026-06-26 19:22:46 -0500

    adding new message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World! Welcome to the Flask application."
+    return "Hello, World! Welcome to the Flask application with a new message."
 
 
 if __name__ == "__main__":
